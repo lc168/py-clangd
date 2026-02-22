2294cd4cc36fcd883a3ea6f426246bb63814b13c
liu cheng <liu@cheng.com>
2026-02-22 13:49:44 +0800

    解决venv的环境安装问题,和venv的git管理问题

--- venv/lib/python3.12/site-packages/pygls/workspace/__init__.py
+++
@@ -1,9 +0,0 @@
-from .workspace import Workspace
-from .text_document import TextDocument
-from .position_codec import PositionCodec
-
-__all__ = (
-    "Workspace",
-    "TextDocument",
-    "PositionCodec",
-)
